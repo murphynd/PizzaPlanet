c5ed227709f6fe61b7b93aec271f31406f854044
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-28 10:35:07 -0700

    this will remove toppings as a prototype method. attempting to collect the checkbox info as an array on input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,62 @@
-function Pizza (userName,size,notes) {
-this.userName: userName;
-this.pizzaPrice: this.price();
-this.size: size;
-this.toppings: this.toppings();
-this.notes: notes
+function Pizza(userName, size, toppings, notes) {
+  this.userName = userName;
+  this.pizzaPrice = this.price();
+  this.size = size;
+  this.toppings = toppings;
+  this.notes = notes;
 }
 //user starts with small cheese pizza at $10
-pizza.prototype.price:
-pizza.prototype.adduser:
-pizza.prototype.toppings:
-pizza.prototype.size:
+pizza.prototype.price = function () {
+  let baseCost = 10;
+  if (this.size === "Medium") {
+    baseCost += 4;
+  }
+  if (this.size === "large") {
+    baseCost += 8;
+  }
+};
 
-function receipt(newOrder){
+function receipt(newOrder) {
   $("#recipt").show();
 
   // can I print just newOrder?
-  newOrder.userName
-  newOrder.pizzaPrice
-  newOrder.size
-  newOrder.toppings
-  newOrder.notes
+  newOrder.userName;
+  newOrder.pizzaPrice;
+  newOrder.size;
+  newOrder.toppings;
+  newOrder.notes;
 }
 
 $(document).ready(function () {
-Document READY 
-newOrder = new Pizza()
-userNameInput :("#Username")
-pizzaSizeInput: ("#pizzaSize")
-toppingsInput: ("#pep")("#pin")("#jal")("#mush")("#olives")
-notesInput: ("#Notes")
+  $("#pizzaOrder").submmit(function (event) {
+    const userNameInput = $("#Username").val();
+    const pizzaSizeInput = $("#pizzaSize").val();
+    const notesInput = $("#Notes").val();
+    let toppingsInput = [];
+    $("input:checkbox[name=type]:checked")
+      .each(function () {
+        toppingsInput.push($(this.val()));
+      })
+      ("#pep")("#pin")("#jal")("#mush")("#olives")
+      .val();
+
+    newOrder = new Pizza(userNameInput, pizzaSizeInput, toppingsInput, notesInput);
+    receipt(newOrder);
+  });
 });
 
+// (form)
+// Change (modifiers)
 
+// - Size of pizza: small medium large (3) (base of 10 plus \$4+ for each to object )
 
+// - Toppings : pepperoni, pinapple, jalapeno, mushroom, peppers (5) (each + 50 cents to object)
 
+// (form)
 
-(form)
-Change (modifiers)
+// Recipt (display to user)
 
-- Size of pizza: small medium large (3) (base of 10 plus \$4+ for each to object )
-
-- Toppings : pepperoni, pinapple, jalapeno, mushroom, peppers (5) (each + 50 cents to object)
-
-(form)
-
-Recipt (display to user)
-
-- Name of user
-- size of pizza with toppings chosen,
-- price for pizza.
-- add time for pick up if possible. (20mins from time entered)
+// - Name of user
+// - size of pizza with toppings chosen,
+// - price for pizza.
+// - add time for pick up if possible. (20mins from time entered)
