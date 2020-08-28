276b623da7a87359819a6a60003d5e8bdc82eaa5
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-28 10:50:19 -0700

    pauses all topping input and javaScript to work ok pizza.prototype.price method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,52 @@
-function Pizza(userName, size, toppings, notes) {
+function Pizza(userName, size, notes) {
   this.userName = userName;
   this.pizzaPrice = this.price();
   this.size = size;
-  this.toppings = toppings;
+  // this.toppings = toppings;
   this.notes = notes;
 }
 //user starts with small cheese pizza at $10
-pizza.prototype.price = function () {
+Pizza.prototype.price = function () {
   let baseCost = 10;
   if (this.size === "Medium") {
     baseCost += 4;
   }
-  if (this.size === "large") {
+  if (this.size === "Large") {
     baseCost += 8;
   }
+  console.log(baseCost);
+  return baseCost;
 };
 
-function receipt(newOrder) {
-  $("#recipt").show();
+// function receipt(newOrder) {
+//   $("#recipt").show();
 
-  // can I print just newOrder?
-  newOrder.userName;
-  newOrder.pizzaPrice;
-  newOrder.size;
-  newOrder.toppings;
-  newOrder.notes;
-}
+//   // can I print just newOrder?
+//   newOrder.userName;
+//   newOrder.pizzaPrice;
+//   newOrder.size;
+//   newOrder.toppings;
+//   newOrder.notes;
+// }
 
 $(document).ready(function () {
-  $("#pizzaOrder").submmit(function (event) {
+  $("#pizzaOrder").submit(function (event) {
+    event.preventDefault();
     const userNameInput = $("#Username").val();
-    const pizzaSizeInput = $("#pizzaSize").val();
+    console.log(userNameInput);
+    let pizzaSizeInput = $("#pizzaSize").val();
+    console.log(pizzaSizeInput);
     const notesInput = $("#Notes").val();
-    let toppingsInput = [];
-    $("input:checkbox[name=type]:checked")
-      .each(function () {
-        toppingsInput.push($(this.val()));
-      })
-      ("#pep")("#pin")("#jal")("#mush")("#olives")
-      .val();
+    // let toppingsInput = [];
+    // $("input:checkbox[name=type]:checked")
+    //   .each(function () {
+    //     toppingsInput.push($(this.val()));
+    //   })("#pep")("#pin")("#jal")("#mush")("#olives")
+    //   .val();
 
-    newOrder = new Pizza(userNameInput, pizzaSizeInput, toppingsInput, notesInput);
-    receipt(newOrder);
+    newOrder = new Pizza(userNameInput, pizzaSizeInput, notesInput);
+    // receipt(newOrder);
+    console.log(newOrder);
   });
 });
 
